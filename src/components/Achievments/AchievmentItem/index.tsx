9211c068ccc5526b44c8a32a341896304c0789ea
Julio Negri <julioyuki@outlook.com>
2024-10-26 06:35:46 -0300

    adding translation to achievments

--- src/components/Achievments/AchievmentItem/index.tsx
+++ src/components/Achievments/AchievmentItem/index.tsx
@@ -1,19 +1,23 @@
 import { Achievment } from "models/achievment"
 
 import "./styles.css";
+import { useTranslation } from "react-i18next";
 
 type AchievmentItemProps = {
     achievment: Achievment
 }
 
 export function AchievmentItem ({ achievment }: AchievmentItemProps) {
+    
+    const { t } = useTranslation();
+    
     return (
         <div className="achievment">
             <div className="icon"></div>
 
             <div className="content">
-                <p className="name">{achievment.name}</p>
-                <p className="description">{achievment.description}</p>
+                <p className="name">{t(achievment.name)}</p>
+                <p className="description">{t(achievment.description)}</p>
             </div>
         </div>
     );
--- src/components/Achievments/index.tsx
+++ src/components/Achievments/index.tsx
@@ -2,50 +2,54 @@ import { Achievment } from "models/achievment"
 import { AchievmentItem } from "./AchievmentItem"
 
 import "./styles.css";
+import { useTranslation } from "react-i18next";
 
 const ACHIEVMENTS: Achievment[] = [
     {
         id: 0,
-        name: "First stage cleared!",
-        description: "Get a promotion!",
+        name: "achievments.first_stage.title",
+        description: "achievments.first_stage.description",
         imageUrl: "",
     },
     {
         id: 1,
-        name: "Second stage cleared!",
-        description: "Get a promotion again!",
+        name: "achievments.second_stage.title",
+        description: "achievments.second_stage.description",
         imageUrl: "",
     },
     {
         id: 2,
-        name: "Base knowledge acquired",
-        description: "Finish the technical school chapter",
+        name: "achievments.knowledge.title",
+        description: "achievments.knowledge.description",
         imageUrl: "",
     },
     {
         id: 3,
-        name: "Not impossible at all",
-        description: "Finish a long book you thought you'd never finish.",
+        name: "achievments.not_impossible.title",
+        description: "achievments.not_impossible.description",
         imageUrl: "",
     },
     {
         id: 4,
-        name: "Worldwide",
-        description: "Work with american clients",
+        name: "achievments.worldwide.title",
+        description: "achievments.worldwide.description",
         imageUrl: "",
     },
     {
         id: 5,
-        name: "Jack-of-all-trades",
-        description: "Work on front end, back end and with infrastructure",
+        name: "achievments.jack.title",
+        description: "achievments.jack.description",
         imageUrl: "",
     }
 ]
 
 export function AchievmentsPage() {
+
+    const { t } = useTranslation();
+
     return (
         <section id="achievmentsSection" className="achievments">
-            <h2>&#x2192; Achievments</h2>
+            <h2>&#x2192; {t("achievments.title")}</h2>
 
             <div className="achievments__list">
                 {
